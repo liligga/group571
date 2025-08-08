7e4fe6e9bc3410e82780453b13c4b9782f7e0533
Igor <igglike@gmail.com>
2025-08-08 18:55:58 +0600

    Сделал инкапсуляцию

--- lesson_3.py
+++ lesson_3.py
@@ -1,2 +1,47 @@
 class Car:
-    pass
+    def __init__(self, max_speed, color, model):
+        self.max_speed = max_speed
+        self.color = color
+        self.model = model
+        self.__fined = False
+
+    def pay_fine(self):
+        # сеттер
+        if self.__fined:
+            self.__fined = False
+
+    def get_fined(self):
+        # геттер
+        return self.__fined
+
+    def drive_to_location(self, location):
+        if self.__fined:
+            raise ValueError(f"Car {self.model} is fined and can not drive anywhere.")
+        print(f"Car {self.model} is driving to {location}")
+
+    def __test(self):
+        print("Private method")
+
+    @property
+    def fined(self):
+        # getter
+        return self.__fined
+
+    @fined.setter
+    def fined(self, value):
+        # setter
+        self.__fined = value
+
+class Truck(Car):
+    def test(self):
+        print(f"Truck test {self.get_fined()}")
+
+car_honda = Car(140, "silver", "Honda Fit")
+car_honda.drive_to_location("Karakol")
+print(car_honda.get_fined())
+print(car_honda.fined)
+car_honda.fined = True
+# car_honda.drive_to_location("Karakol")
+truck_man = Truck(200, "grey", "MAN")
+truck_man.test()
+print(car_honda._Car__fined)
---
+++ lesson_3_1.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+class Dog(Animal):
+    def make_sound(self):
+        print("гав гав")
+
+puppy = Dog()
+puppy.make_sound()
