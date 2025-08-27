a75bba78eadfbe6b69008e0ec28337a27d0ef998
Igor <igglike@gmail.com>
2025-08-27 11:23:30 +0600

    :package:Добавил с order by, limit, foreign key:package:

--- lessons/lesson_7.py
+++ lessons/lesson_7.py
@@ -22,6 +22,18 @@ def delete_student(student_id):
     conn.execute("DELETE FROM students WHERE id = ?", (student_id,))
     conn.commit()
 
+def get_students():
+    result = conn.execute("SELECT * FROM students ORDER BY age DESC, city LIMIT 2")
+    return result
+
+def get_student_by_age(age):
+    result = conn.execute(
+        "SELECT id, name FROM students WHERE age = ?",
+        (age,)
+    )
+    return result
+
+
 if __name__ == '__main__':
     conn = sqlite3.connect('database.db')
 
@@ -29,5 +41,18 @@ if __name__ == '__main__':
 
     add_student("John", 20, "New York")
     add_student("Aibek", 22, "Karakol")
+    add_student("Aisuluu", 19, "Karakol")
+    add_student("Ariana", 19, "Bishkek")
 
-    delete_student(1)
+    for st in get_students():
+        print(st)
+    print("----------------")
+
+    # delete_student(1)
+
+    for st in get_students():
+        print(st)
+
+    print("--------------")
+    for st in get_student_by_age(20):
+        print(st)
